17d61d3ad7f200eab7359abb9f9b22a321d6fb16
Sydney205 <diekechi@gmail.com>
2026-07-04 02:06:32 +0100

    timeout

--- utils/mailer.js
+++ utils/mailer.js
@@ -5,7 +5,11 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  // These settings prevent the server from hanging indefinitely
+  connectionTimeout: 10000, // 10 seconds
+  greetingTimeout: 10000,
+  socketTimeout: 10000
 });
 
 module.exports = transporter;
